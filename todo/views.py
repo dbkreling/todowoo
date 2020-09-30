2e7ed5d111283c7270c5ff6c74d757fdf71c0da0
Daniel Kreling <dkreling@redhat.com>
2020-09-30 23:09:22 +0200

    lecture 63: add IntegrityError to make usernames unique
    A simple try/catch error with a IntegrityError makes the usernames to be unique
    and no duplicates to be added to the server.
    
    Signed-off-by: Daniel Kreling <dkreling@redhat.com>

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 def signupuser(request):
@@ -8,7 +9,10 @@ def signupuser(request):
         return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-            user.save()
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error':"ERROR: username already exists."})
         else:
             return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error':"ERROR: Passwords do not match"})
