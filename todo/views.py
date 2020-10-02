9462378a529c0bb3433aa1f5d8a124d518465f4f
Daniel Kreling <dkreling@redhat.com>
2020-10-02 10:16:29 +0200

    refactor for django standards
    Create class Signup extending View. It will contain 'template_name' and
    'form_class' that are standard names from View. All 'todo/signupuser.html'
    are then replaced by a more standardized 'self.template_name'.
    
    The View class already contains all the checks and error messages used in
    this project so far, inside the function 'locals()', so we can safely
    remove the custom error messages we created before.
    
    Inside Signup, the 'post' function will verify if the passwords satisfy all
    the conditions the UserCreationForm class require and will return any error
    messages to locals(), passed to the signupuser.html template inside the
    'from.non_field_errors' list of errors, if any.
    
    We then save the form for the user and log them in, redirecting the current
    todos template as a return from the function.
    
    Signed-off-by: Daniel Kreling <dkreling@redhat.com>

--- todo/views.py
+++ todo/views.py
@@ -3,22 +3,25 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.db import IntegrityError
+from django.views import View
 
 # Create your views here.
-def signupuser(request):
-    if request.method == 'GET':
-        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
-    else:
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-                user.save()
-                login(request, user)
-                return redirect('currenttodos')
-            except IntegrityError:
-                return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error':"ERROR: username already exists."})
-        else:
-            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error':"ERROR: Passwords do not match"})
+class Signup(View):
+    template_name="todo/signupuser.html"
+    form_class=UserCreationForm
+
+    def get(self, request):
+        return render(request, self.template_name, {'form': self.form_class()})
+
+    def post(self, request):
+        form=self.form_class(data=request.POST)
+
+        if not form.is_valid():
+            return render(request, self.template_name, locals())
+
+        user = form.save()
+        login(request, user)
+        return redirect('currenttodos')
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
