2cff151cf3acc158fbe772ceb7aa9f4ae544d1e5
Daniel Kreling <dkreling@redhat.com>
2020-11-29 16:55:36 +0100

    lecture 66: add a login feature
    To add a login, we follow the basics of signing up a user abut rather than create
    the user, we authenticate it using the libraries imported to views.py.
    
    Also fix the Signup and Login links in the Home page to make them functional

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 
 # Create your views here.
@@ -22,6 +22,18 @@ def signupuser(request):
                 return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error':"ERROR: username already exists."})
         else:
             return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error':"ERROR: Passwords do not match"})
+
+def loginuser (request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error':'Username and password did not match'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
     path('currenttodos/', views.currenttodos, name='currenttodos'),
 ]
